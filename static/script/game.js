dda7a266b7f39d1befeac723db587d07ef0d770f
philbadcode <phillip.hail@yahoo.com>
2023-12-09 14:42:52 -0600

    added some more function

--- static/script/game.js
+++ static/script/game.js
@@ -1,29 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
     const balloon = document.getElementById('balloon');
 
-    balloon.addEventListener('mouseenter', bounceAway);
+    let velocity = { x: 0, y: 0 };
+    const gravity = 0.1;
+    const maxSpeed = 5; // Adjust the max speed as needed
 
-    function bounceAway(e) {
-        const balloonRect = balloon.getBoundingClientRect();
+    balloon.addEventListener('mouseenter', applyMouseForce);
+
+    function applyMouseForce(e) {
         const mouseX = e.clientX;
         const mouseY = e.clientY;
 
-        // Calculate the distance and direction between the mouse and the center of the balloon
-        const deltaX = mouseX - (balloonRect.left + balloonRect.width / 2);
-        const deltaY = mouseY - (balloonRect.top + balloonRect.height / 2);
+        // Calculate the distance and direction between the mouse and the balloon
+        const deltaX = mouseX - balloon.getBoundingClientRect().left;
+        const deltaY = mouseY - balloon.getBoundingClientRect().top;
 
-        // Calculate the new position based on the distance and direction
-        const newX = balloonRect.left - deltaX;
-        const newY = balloonRect.top - deltaY;
+        // Adjust the velocity based on the mouse force
+        velocity.x += deltaX * 0.01;
+        velocity.y += deltaY * 0.01;
+    }
 
-        // Update the balloon's position and add bounce animation
+    function updateBalloon() {
+        // Apply gravity
+        velocity.y += gravity;
+
+        // Limit the speed
+        const speed = Math.sqrt(velocity.x ** 2 + velocity.y ** 2);
+        if (speed > maxSpeed) {
+            const ratio = maxSpeed / speed;
+            velocity.x *= ratio;
+            velocity.y *= ratio;
+        }
+
+        // Update position based on velocity
+        const balloonRect = balloon.getBoundingClientRect();
+        const newX = balloonRect.left + velocity.x;
+        const newY = balloonRect.top + velocity.y;
+
         balloon.style.left = `${newX}px`;
         balloon.style.top = `${newY}px`;
-        balloon.classList.add('bounce-animation');
 
-        // Remove the bounce animation after it completes
-        setTimeout(() => {
-            balloon.classList.remove('bounce-animation');
-        }, 500);
+        // Bounce off the walls
+        if (newX < 0 || newX + balloonRect.width > window.innerWidth) {
+            velocity.x *= -1;
+        }
+        if (newY < 0 || newY + balloonRect.height > window.innerHeight) {
+            velocity.y *= -1;
+        }
+
+        requestAnimationFrame(updateBalloon);
     }
+
+    // Start the animation loop
+    updateBalloon();
 });
