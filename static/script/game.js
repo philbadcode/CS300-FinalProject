be2915ffd44b74ec59e9b34e9a06497ca8217fc2
philbadcode <phillip.hail@yahoo.com>
2023-12-09 15:21:29 -0600

    working game

--- static/script/game.js
+++ static/script/game.js
@@ -1,77 +1,77 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const balloon = document.getElementById('balloon');
-    let velocityX = 0;
-    let velocityY = 0;
-    const friction = 0.95;
-    const pushStrength = 20;
-    const gravity = 0.18;
-  
-    let mouseX = 0;
-    let mouseY = 0;
-    let lastCollisionTime = 0;
-    const collisionCooldown = 500; // Cooldown time in milliseconds
-  
-    function startBalloonPosition() {
-      const startX = window.innerWidth / 2 - balloon.clientWidth / 2;
-      const startY = window.innerHeight / 2 - balloon.clientHeight / 2;
-      setBalloonPosition(startX, startY);
+  const balloon = document.getElementById('balloon');
+  let velocityX = 0;
+  let velocityY = 0;
+  const friction = 0.95;
+  const pushStrength = 20;
+  const gravity = 0.18;
+
+  let mouseX = 0;
+  let mouseY = 0;
+  let lastCollisionTime = 0;
+  const collisionCooldown = 500; // Cooldown time in milliseconds
+
+  function startBalloonPosition() {
+    const startX = window.innerWidth / 2 - balloon.clientWidth / 2;
+    const startY = window.innerHeight / 2 - balloon.clientHeight / 2;
+    setBalloonPosition(startX, startY);
+  }
+
+  function setBalloonPosition(x, y) {
+    balloon.style.left = x + 'px';
+    balloon.style.top = y + 'px';
+  }
+
+  function checkCollision() {
+    let now = Date.now();
+    if (now - lastCollisionTime < collisionCooldown) return;
+
+    let rect = balloon.getBoundingClientRect();
+    let balloonCenterX = rect.left + rect.width / 2;
+    let balloonCenterY = rect.top + rect.height / 2;
+
+    if (distance(mouseX, mouseY, balloonCenterX, balloonCenterY) < rect.width / 2) {
+      let angle = Math.atan2(balloonCenterY - mouseY, balloonCenterX - mouseX);
+      velocityX += pushStrength * Math.cos(angle);
+      velocityY += pushStrength * Math.sin(angle);
+      lastCollisionTime = now;
     }
-  
-    function setBalloonPosition(x, y) {
-      balloon.style.left = x + 'px';
-      balloon.style.top = y + 'px';
+  }
+
+  function update() {
+    velocityX *= friction;
+    velocityY *= friction;
+    velocityY += gravity;
+
+    checkCollision();
+
+    let rect = balloon.getBoundingClientRect();
+    let newX = rect.left + velocityX;
+    let newY = rect.top + velocityY;
+
+    if (newX + rect.width > window.innerWidth || newX < 0) {
+      velocityX *= -1;
+      newX = newX < 0 ? 0 : window.innerWidth - rect.width;
     }
-  
-    function checkCollision() {
-      let now = Date.now();
-      if (now - lastCollisionTime < collisionCooldown) return;
-  
-      let rect = balloon.getBoundingClientRect();
-      let balloonCenterX = rect.left + rect.width / 2;
-      let balloonCenterY = rect.top + rect.height / 2;
-  
-      if (distance(mouseX, mouseY, balloonCenterX, balloonCenterY) < rect.width / 2) {
-        let angle = Math.atan2(balloonCenterY - mouseY, balloonCenterX - mouseX);
-        velocityX += pushStrength * Math.cos(angle);
-        velocityY += pushStrength * Math.sin(angle);
-        lastCollisionTime = now;
-      }
+
+    if (newY > window.innerHeight) {
+      window.location.href = '/lose';
+      return;
     }
-  
-    function update() {
-      velocityX *= friction;
-      velocityY *= friction;
-      velocityY += gravity;
-  
-      checkCollision();
-  
-      let rect = balloon.getBoundingClientRect();
-      let newX = rect.left + velocityX;
-      let newY = rect.top + velocityY;
-  
-      if (newX + rect.width > window.innerWidth || newX < 0) {
-        velocityX *= -1;
-        newX = newX < 0 ? 0 : window.innerWidth - rect.width;
-      }
-  
-      if (newY > window.innerHeight) {
-        window.location.href = 'lose.html';
-        return;
-      }
-  
-      setBalloonPosition(newX, newY);
-      requestAnimationFrame(update);
-    }
-  
-    function distance(x1, y1, x2, y2) {
-      return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-    }
-  
-    document.addEventListener('mousemove', (e) => {
-      mouseX = e.clientX;
-      mouseY = e.clientY;
-    });
-  
-    startBalloonPosition();
-    update();
-});  
+
+    setBalloonPosition(newX, newY);
+    requestAnimationFrame(update);
+  }
+
+  function distance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  }
+
+  document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  startBalloonPosition();
+  update();
+});
